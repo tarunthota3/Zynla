0c3c22abbe66bf7a72726f16879d90f895bf4bcc
tarunthota3 <tarunthota3@gmail.com>
2017-10-15 23:24:07 +0530

    neo4j configuration for seneca updated

--- senecaMicroService/config/neo4j.js
+++ senecaMicroService/config/neo4j.js
@@ -1,5 +1,5 @@
 module.exports = (function() {
   let neo4j = require('neo4j-driver').v1;
-  let driver = neo4j.driver('bolt://172.23.238.220', neo4j.auth.basic('neo4j', '9455338161'));
+  let driver = neo4j.driver('bolt://52.14.238.159', neo4j.auth.basic('neo4j', '9455338161'));
   return driver;
 }());
